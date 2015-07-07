43bbcdf3fc292b69b30c5b87346216146e7dae35
cedar101 <cedar101@gmail.com>
2015-07-07 20:43:00 +0900

    share.install "*"

--- Library/Formula/mecab-ko-dic.rb
+++ Library/Formula/mecab-ko-dic.rb
@@ -5,7 +5,9 @@ class MecabKoDic < Formula
   homepage "https://bitbucket.org/eunjeon/mecab-ko-dic"
   url "https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-2.0.1-20150707.tar.gz"
   sha1 "f6987b1a9b728e375d05764286fb5a97f127aa42"
-
+  
+  buildpath 
+  
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on 'mecab-ko'
@@ -20,6 +22,6 @@ class MecabKoDic < Formula
       open("#{etc}/mecabrc", "a") { |f| f.puts "dicdir = #{opt_prefix}\n" }
     end
     
-    prefix.install Dir["tools/*"]
+    share.install "*"
   end
 end
